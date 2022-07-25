deb28eec0ee9447574ac581e20e80b02d112de81
Myshiak.su <67818235+Myshiak@users.noreply.github.com>
2022-07-25 21:00:44 +0300

    Update life.c

--- life.c
+++ life.c
@@ -17,8 +17,9 @@
 #define seed 21
 #define dead '.'
 #define alive 'O'
+#define randomly false
 // functions and fields
-#define time_delay (size + 250)
+#define time_delay (size + 300)
 #define get_by(x, y) table[y][x]
 #define set_alive(x, y) table[y][x] = alive
 #define set_dead(x, y) table[y][x] = dead
@@ -39,42 +40,37 @@ int alive_neighbours(int x, int y) {
     int res = 0;
     if (x - 1 >= 0 && y - 1 >= 0 && is_alive(x - 1, y - 1)) res++;
     if (y - 1 >= 0 && is_alive(x, y - 1)) res++;
-    if (x + 1 < size && y - 1 >= 0 && is_alive(x + 1, y - 1)) res++;
-    if (x + 1 < size && is_alive(x + 1, y)) res++;
-    if (x + 1 < size && y + 1 < size && is_alive(x + 1, y + 1)) res++;
-    if (y + 1 < size && is_alive(x, y + 1)) res++;
-    if (x - 1 >= 0 && y + 1 < size && is_alive(x - 1, y + 1)) res++;
+    if (x + 1 < (int)sqrt(size) && y - 1 >= 0 && is_alive(x + 1, y - 1)) res++;
+    if (x + 1 < (int)sqrt(size) && is_alive(x + 1, y)) res++;
+    if (x + 1 < (int)sqrt(size) && y + 1 < (int)sqrt(size) && is_alive(x + 1, y + 1)) res++;
+    if (y + 1 < (int)sqrt(size) && is_alive(x, y + 1)) res++;
+    if (x - 1 >= 0 && y + 1 < (int)sqrt(size) && is_alive(x - 1, y + 1)) res++;
     if (x - 1 >= 0 && is_alive(x - 1, y)) res++;
     return res;
-}
-// for main cycle
-bool is_all_dead() {
-    for (int y = 0; y < sqrt(size); y++) {
-        for (int x = 0; x < sqrt(size); x++) {
-            if (is_alive(x, y)) {
-                return false;
-            }
-        }
-    }
-    return true;
 }
 // main function
 int main() {
     for (int y = 0; y < sqrt(size); y++) {
         for (int x = 0; x < sqrt(size); x++) {
-            if (rand()%100+1 <= seed) {
+            if (randomly && rand() % 100 + 1 <= seed) {
                 set_alive(x, y);
             } else {
                 set_dead(x, y);
             }
         }
     }
-    while (!is_all_dead()) {
+    if (!randomly) {
+    	// custom generation
+    	set_alive(10, 10);
+    	set_alive(10, 11);
+    	set_alive(10, 12);
+    }
+    while (true) {
         for (int y = 0; y < sqrt(size); y++) {
             for (int x = 0; x < sqrt(size); x++) {
-                if (alive_neighbours(x, y) == 3 && !is_alive(x, y)) {
+                if (alive_neighbours(x, y) == 3) {
                     set_alive(x, y);
-                } else if ((alive_neighbours(x, y)>3 || alive_neighbours(x, y)<2) && is_alive(x, y)) {
+                } else if (alive_neighbours(x, y)!=3 && alive_neighbours(x, y)!=2 && is_alive(x, y)) {
                     set_dead(x, y);
                 }
                 print_table();

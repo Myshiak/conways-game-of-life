fe1c956a4f3250f2861f304c9ef87f47d71334e5
Myshiak.su <67818235+Myshiak@users.noreply.github.com>
2022-07-28 18:28:17 +0300

    Update life.c

--- life.c
+++ life.c
@@ -1,83 +1,74 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
-#include <time.h>
 #include <stdlib.h>
-#ifdef _WIN32
-    #include <windows.h>
-    #define sleep_ms(ms) Sleep(ms / 1000)
-    #define clear_console() system("cls")
-#else
-    #include <unistd.h>
-    #define sleep_ms(ms) usleep(ms)
-    #define clear_console() printf("\e[1;1H\e[2J")
-#endif
-// config
-#define size 400
-#define seed 21
+#include <unistd.h>
+#define sizex 22 // config
+#define sizey 10
+#define seed 25
 #define dead '.'
-#define alive 'O'
-#define randomly false
-// functions and fields
-#define time_delay (size + 300)
-#define get_by(x, y) table[y][x]
+#define alive 'o'
+#define randomly true
+#define colored true
+#define get_by(x, y) table[y][x] //functions and fields
+#define is_alive(x, y) (table[y][x] == alive)
 #define set_alive(x, y) table[y][x] = alive
 #define set_dead(x, y) table[y][x] = dead
-#define is_alive(x, y) (table[y][x] == alive)
-// main table
-char table[size][size];
-// prints the table
-void print_table() {
-    for (int y = 0; y < sqrt(size); y++) {
-        for (int x = 0; x < sqrt(size); x++) {
-        	printf("%c ", get_by(x, y));
-        }
-        printf("\n");
-    }
+char table[sizey][sizex];
+void print_table(int X,int Y){
+	for(int y=0;y<sizey;y++) {
+		for(int x=0;x<sizex;x++) {
+			if (x != X && y != Y && colored) {
+				if (is_alive(x,y)){
+					printf("\e[31m%c \e[0m", get_by(x,y));
+				} else {
+					printf("\e[34m%c \e[0m", get_by(x,y));
+				}
+			} else {
+				printf("%c ", get_by(x,y));
+			}
+		}
+		printf("\n");
+	}
 }
-// counts alive neighbours
-int alive_neighbours(int x, int y) {
-    int res = 0;
-    if (x - 1 >= 0 && y - 1 >= 0 && is_alive(x - 1, y - 1)) res++;
-    if (y - 1 >= 0 && is_alive(x, y - 1)) res++;
-    if (x + 1 < (int)sqrt(size) && y - 1 >= 0 && is_alive(x + 1, y - 1)) res++;
-    if (x + 1 < (int)sqrt(size) && is_alive(x + 1, y)) res++;
-    if (x + 1 < (int)sqrt(size) && y + 1 < (int)sqrt(size) && is_alive(x + 1, y + 1)) res++;
-    if (y + 1 < (int)sqrt(size) && is_alive(x, y + 1)) res++;
-    if (x - 1 >= 0 && y + 1 < (int)sqrt(size) && is_alive(x - 1, y + 1)) res++;
-    if (x - 1 >= 0 && is_alive(x - 1, y)) res++;
-    return res;
+int alive_nb(int x, int y) {
+	int nbs[8][2]={{-1,-1},{0,-1},{1,-1},{1,0},{1,1},{0,1},{-1,1},{-1,0}};
+	int	res = 0, nbx, nby;
+	for (int k=0;k<8;k++) {
+		nbx = x + nbs[k][0]; nby = y + nbs[k][1];
+		if(nbx >= 0 && nbx < sizex &&
+		   nby >= 0 && nby < sizey &&
+			is_alive(nbx,nby)) {
+			res++;
+		}
+	}
+	return res;
 }
-// main function
 int main() {
-    for (int y = 0; y < sqrt(size); y++) {
-        for (int x = 0; x < sqrt(size); x++) {
-            if (randomly && rand() % 100 + 1 <= seed) {
-                set_alive(x, y);
-            } else {
-                set_dead(x, y);
-            }
-        }
-    }
-    if (!randomly) {
-    	// custom generation
-    	set_alive(10, 10);
-    	set_alive(10, 11);
-    	set_alive(10, 12);
-    }
-    while (true) {
-        for (int y = 0; y < sqrt(size); y++) {
-            for (int x = 0; x < sqrt(size); x++) {
-                if (alive_neighbours(x, y) == 3) {
-                    set_alive(x, y);
-                } else if (alive_neighbours(x, y)!=3 && alive_neighbours(x, y)!=2 && is_alive(x, y)) {
-                    set_dead(x, y);
-                }
-                print_table();
-                sleep_ms(time_delay);
-                clear_console();
-            }
-        }
-    }
-    return 0;
+	for(int y=0;y<sizey;y++) {
+		for(int x=0;x<sizex;x++) {
+			if (randomly&&rand()%100+1<=seed) {
+				set_alive(x,y);
+			} else {
+				set_dead(x, y);
+			}
+		}
+	}
+	if (!randomly) {}
+	while (true) {
+		for(int y=0;y<sizey;y++) {
+			for(int x=0;x<sizex;x++) {
+				printf("(%d,%d); AN:%d\n",x,y,alive_nb(x,y));
+				if (alive_nb(x,y) == 3) {
+					set_alive(x, y);
+				} else if (alive_nb(x,y) != 2 &&
+							alive_nb(x,y) !=3 &&
+							is_alive(x,y)) {
+					set_dead(x, y);
+				}
+				print_table(x,y);
+				usleep(800);
+				printf("\e[1;1H\e[2J");
+			}
+		}
+	}
 }
